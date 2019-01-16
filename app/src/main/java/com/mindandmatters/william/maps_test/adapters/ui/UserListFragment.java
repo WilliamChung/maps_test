09c91c047c6b53f59de4140d114e009e522c23b4
William Chung <williamc825@hotmail.com>
2019-01-16 13:38:54 -0800

    Finished video 10:  Map window now moves to user location

--- app/src/main/java/com/mindandmatters/william/maps_test/adapters/ui/UserListFragment.java
+++ app/src/main/java/com/mindandmatters/william/maps_test/adapters/ui/UserListFragment.java
@@ -15,11 +15,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
 import com.mindandmatters.william.maps_test.R;
 import com.mindandmatters.william.maps_test.adapters.UserRecyclerAdapter;
 import com.mindandmatters.william.maps_test.models.User;
@@ -42,6 +45,9 @@ public class UserListFragment extends Fragment implements OnMapReadyCallback {
     private ArrayList<User> mUserList = new ArrayList<>();
     private ArrayList<UserLocation> mUserLocations = new ArrayList<>();
     private UserRecyclerAdapter mUserRecyclerAdapter;
+    private GoogleMap mGoogleMap;
+    private UserLocation mUserPosition;
+    private LatLngBounds mMapBoundary;
 
 
     public static UserListFragment newInstance() {
@@ -67,13 +73,17 @@ public class UserListFragment extends Fragment implements OnMapReadyCallback {
         initUserListRecyclerView();
         initGoogleMap(savedInstanceState);
 
-        for(UserLocation userLocation: mUserLocations){
-            Log.d(TAG, "onCreateView: user location: " + userLocation.getUser().getUsername());
-            Log.d(TAG, "onCreateView: geopoint: " + userLocation.getGeo_point().getLatitude() + ", " +
-            userLocation.getGeo_point().getLongitude());
-        }
+        setUserPosition();
 
         return view;
+    }
+
+    private void setUserPosition() {
+        for (UserLocation userLocation : mUserLocations) {
+            if (userLocation.getUser().getUser_id().equals(FirebaseAuth.getInstance().getUid())) {
+                mUserPosition = userLocation;
+            }
+        }
     }
 
     private void initGoogleMap(Bundle savedInstanceState) {
@@ -132,16 +142,28 @@ public class UserListFragment extends Fragment implements OnMapReadyCallback {
         map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
         if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) !=
                 PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+
             return;
         }
         map.setMyLocationEnabled(true);
+        mGoogleMap = map;
+        setCameraView();
+    }
+
+    private void setCameraView() {
+
+        // Set a boundary to start
+        double bottomBoundary = mUserPosition.getGeo_point().getLatitude() - .1;
+        double leftBoundary = mUserPosition.getGeo_point().getLongitude() - .1;
+        double topBoundary = mUserPosition.getGeo_point().getLatitude() + .1;
+        double rightBoundary = mUserPosition.getGeo_point().getLongitude() + .1;
+
+        mMapBoundary = new LatLngBounds(
+                new LatLng(bottomBoundary, leftBoundary),
+                new LatLng(topBoundary, rightBoundary)
+        );
+
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(mMapBoundary, 0));
     }
 
     @Override
