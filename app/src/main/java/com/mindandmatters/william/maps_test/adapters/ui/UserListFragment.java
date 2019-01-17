20a1b44ded714c69b2174b6a472ded8b6c2bc628
William Chung <williamc825@hotmail.com>
2019-01-16 16:16:14 -0800

    Finished video 16:  Map now expands and contracts on click

--- app/src/main/java/com/mindandmatters/william/maps_test/adapters/ui/UserListFragment.java
+++ app/src/main/java/com/mindandmatters/william/maps_test/adapters/ui/UserListFragment.java
@@ -1,6 +1,7 @@
 package com.mindandmatters.william.maps_test.adapters.ui;
 
 import android.Manifest;
+import android.animation.ObjectAnimator;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,6 +15,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -36,21 +38,27 @@ import com.mindandmatters.william.maps_test.models.ClusterMarker;
 import com.mindandmatters.william.maps_test.models.User;
 import com.mindandmatters.william.maps_test.models.UserLocation;
 import com.mindandmatters.william.maps_test.util.MyClusterManagerRenderer;
+import com.mindandmatters.william.maps_test.util.ViewWeightAnimationWrapper;
 
 import java.util.ArrayList;
 
 import static com.mindandmatters.william.maps_test.Constants.MAPVIEW_BUNDLE_KEY;
 
-public class UserListFragment extends Fragment implements OnMapReadyCallback {
-
-    private static final String TAG = "UserListFragment";
+public class UserListFragment extends Fragment implements
+        OnMapReadyCallback,
+        View.OnClickListener {
 
     //constants
+    private static final String TAG = "UserListFragment";
     private static final int LOCATION_UPDATE_INTERVAL = 3000;
+    private static final int MAP_LAYOUT_STATE_CONTRACTED = 0;
+    private static final int MAP_LAYOUT_STATE_EXPANDED = 1;
+
 
     //widgets
     private RecyclerView mUserListRecyclerView;
     private MapView mMapView;
+    private RelativeLayout mMapContainer;
 
 
     //vars
@@ -65,7 +73,7 @@ public class UserListFragment extends Fragment implements OnMapReadyCallback {
     private ArrayList<ClusterMarker> mClusterMarkers = new ArrayList<>();
     private Handler mHandler = new Handler();
     private Runnable mRunnable;
-
+    private int mMapLayoutState = 0;
 
     public static UserListFragment newInstance() {
         return new UserListFragment();
@@ -86,6 +94,9 @@ public class UserListFragment extends Fragment implements OnMapReadyCallback {
         View view = inflater.inflate(R.layout.fragment_user_list, container, false);
         mMapView = (MapView) view.findViewById(R.id.user_list_map);
         mUserListRecyclerView = view.findViewById(R.id.user_list_recycler_view);
+        mMapContainer = view.findViewById(R.id.map_container);
+
+        view.findViewById(R.id.btn_full_screen_map).setOnClickListener(this);
 
         initUserListRecyclerView();
         initGoogleMap(savedInstanceState);
@@ -94,6 +105,8 @@ public class UserListFragment extends Fragment implements OnMapReadyCallback {
 
         return view;
     }
+
+
 
     private void startUserLocationsRunnable(){
         Log.d(TAG, "startUserLocationsRunnable: starting runnable for retrieving updated locations.");
@@ -321,6 +334,63 @@ public class UserListFragment extends Fragment implements OnMapReadyCallback {
     public void onLowMemory() {
         super.onLowMemory();
         mMapView.onLowMemory();
+    }
+
+    private void expandMapAnimation(){
+        ViewWeightAnimationWrapper mapAnimationWrapper = new ViewWeightAnimationWrapper(mMapContainer);
+        ObjectAnimator mapAnimation = ObjectAnimator.ofFloat(mapAnimationWrapper,
+                "weight",
+                50,
+                100);
+        mapAnimation.setDuration(800);
+
+        ViewWeightAnimationWrapper recyclerAnimationWrapper = new ViewWeightAnimationWrapper(mUserListRecyclerView);
+        ObjectAnimator recyclerAnimation = ObjectAnimator.ofFloat(recyclerAnimationWrapper,
+                "weight",
+                50,
+                0);
+        recyclerAnimation.setDuration(800);
+
+        recyclerAnimation.start();
+        mapAnimation.start();
+    }
+
+    private void contractMapAnimation(){
+        ViewWeightAnimationWrapper mapAnimationWrapper = new ViewWeightAnimationWrapper(mMapContainer);
+        ObjectAnimator mapAnimation = ObjectAnimator.ofFloat(mapAnimationWrapper,
+                "weight",
+                100,
+                50);
+        mapAnimation.setDuration(800);
+
+        ViewWeightAnimationWrapper recyclerAnimationWrapper = new ViewWeightAnimationWrapper(mUserListRecyclerView);
+        ObjectAnimator recyclerAnimation = ObjectAnimator.ofFloat(recyclerAnimationWrapper,
+                "weight",
+                0,
+                50);
+        recyclerAnimation.setDuration(800);
+
+        recyclerAnimation.start();
+        mapAnimation.start();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_full_screen_map:{
+
+                if(mMapLayoutState == MAP_LAYOUT_STATE_CONTRACTED){
+                    mMapLayoutState = MAP_LAYOUT_STATE_EXPANDED;
+                    expandMapAnimation();
+                }
+                else if(mMapLayoutState == MAP_LAYOUT_STATE_EXPANDED){
+                    mMapLayoutState = MAP_LAYOUT_STATE_CONTRACTED;
+                    contractMapAnimation();
+                }
+                break;
+            }
+
+        }
     }
 }
 
